2c81c36af6a3cf384d249b8fba2fc49248f65600
aglaya <aglayastunkus1@gmail.com>
2021-11-03 22:19:30 +0300

    feat: add the 01-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+const {stdin, stdout} = process;
+
+const rl = readline.createInterface(
+    process.stdin, process.stdout);
+
+const fs = require('fs');
+const path = require('path');
+
+
+rl.setPrompt(`You can write whatever you want!\n`);
+rl.prompt();
+rl.on('line', (text) => {
+    if (text === 'exit') {
+        rl.close()
+    } else {
+        fs.appendFile(
+            path.join(__dirname, 'text.txt'),
+            `${text}\n`,
+            err => {
+                if (err) throw err;
+            }
+        );
+    }
+});
+process.on('exit', () => stdout.write('Good luck!'));
+
